1dda945da2b007ed4485ed25863b050a438161a6
wentianlin <7989363@qq.com>
2017-10-25 21:29:03 +0800

    add login activity.

--- app/src/main/java/com/example/wentianlin/idorm/LoginActivity.java
+++ app/src/main/java/com/example/wentianlin/idorm/LoginActivity.java
@@ -1,13 +1,105 @@
 package com.example.wentianlin.idorm;
 
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+public class LoginActivity extends Activity implements View.OnClickListener {
+    private static final int CHECK_NUM_AND_PWD = 1;
+    private ImageView loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginBtn = (ImageView) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(this);
+
     }
+
+    private Handler mHandler = new Handler(){
+        public void handleMessage(android.os.Message msg){
+            switch (msg.what) {
+                case CHECK_NUM_AND_PWD:
+                    if(msg.obj.equals("1")){
+
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this,"学号或密码错误！", Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    //点击事件监听函数
+    @Override
+    public void onClick(View v) {
+        //点击登录按钮
+        if(v.getId() == R.id.loginBtn){
+            //获取输入
+            EditText numEdit = (EditText)findViewById(R.id.numEditText);
+            EditText pwdEdit= (EditText)findViewById(R.id.pwdEditText);
+            String numText = numEdit.getText().toString();
+            String pwdText = pwdEdit.getText().toString();
+            Log.d("debug",numText);
+            Log.d("debug",pwdText);
+            checkNumPwd(numText,pwdText);
+        }
+    }
+
+    //验证账号密码
+    private void checkNumPwd(String num, String pwd){
+        final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=101010100";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection con=null;
+                try{
+                    URL url = new URL(address);
+                    con = (HttpURLConnection)url.openConnection( );
+                    con.setRequestMethod("GET");
+                    con.setConnectTimeout(8000);
+                    con.setReadTimeout(8000);
+                    InputStream in = con.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder() ;
+                    String str;
+                    while((str=reader.readLine()) != null){
+                        response.append(str);
+                        Log.d("debug", str);
+                    }
+                    String responseStr=response.toString();
+                    Message msg =new Message();
+                    msg.what = CHECK_NUM_AND_PWD;
+                    msg.obj= responseStr;
+                    mHandler.sendMessage(msg);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+
 }
